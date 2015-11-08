2ad799e85a190cd05dd9aac81646b1912b42f1b0
thienquang <quangthien@outlook.com>
2015-11-08 22:52:35 +0700

    Initial Commit

---
+++ Bow Ties/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bow Ties
+//
+//  Created by ThienQuang on 11/8/15.
+//  Copyright © 2015 quangthien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
